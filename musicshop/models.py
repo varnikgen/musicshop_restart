67028a52259b82c98b893151808192f680ea6da2
Nikolay Varfolomeev <varnikgen@yandex.ru>
2021-12-12 00:58:32 +1000

    New models chapter 2

--- musicshop/models.py
+++ musicshop/models.py
@@ -1,4 +1,7 @@
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
+
 
 
 class MediaType(models.Model):
@@ -67,8 +70,61 @@ class Album(models.Model):
     song_list = models.TextField(verbose_name="Трэклист")
     release_date = models.DateField(verbose_name="Дата релиза")
     slug = models.SlugField()
-    description = models.TextField(verbose_name="Описание", )
+    description = models.TextField(verbose_name="Описание", default="Описание появится позже")
+    stock = models.IntegerField(default=1, verbose_name="Наличие на складе")
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
+    offer_of_the_week = models.BooleanField(default=False, verbose_name="Предложение недели?")
 
     def __str__(self):
-        return self.name
-    
+        return f"{self.id} | {self.artist.name} | {self.name}"
+
+    @property
+    def ct_model(self):
+        return self._meta.model_name
+
+    class Meta:
+        verbose_name = 'Альбом'
+        verbose_name_plural = 'Альбомы'
+
+
+class CartProduct(models.Model):
+    """Продукт корзины"""
+
+    user = models.ForeignKey('Customer', verbose_name="Покупатель", on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name="Корзина", on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    qty = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
+
+    def __str__(self):
+        return f"Продукт: {self.content_object.name} (для корзины)"
+
+    def save(self, *args, **kwargs):
+        self.final_price = self.qty * self.content_object.price
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Продукт корзины"
+        verbose_name_plural = "Продукты корзины"
+
+
+class Cart(models.Model):
+    """Корзина"""
+
+    owner = models.ForeignKey("Customer", verbose_name="Покупатель", on_delete=models.CASCADE)
+    products = models.ManyToManyField(
+        CartProduct, blank=True, null=True, related_name="related_cart", verbose_name="Продукты для корзины"
+    )
+    total_products = models.IntegerField(default=0, verbose_name="Общее кол-во товара")
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
+    in_order = models.BooleanField(default=False)
+    for_anonymous_user = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзины"
