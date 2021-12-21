082a73f9f3921e68077c8ede11cf053775be6af2
Nikolay Varfolomeev <varnikgen@yandex.ru>
2021-12-21 10:56:22 +1000

    add templates for login and registration

--- musicshop/urls.py
+++ musicshop/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import BaseView, ArtistDetailView, AlbumDetailView
+from .views import BaseView, ArtistDetailView, AlbumDetailView, RegistrationView, LoginView
 
 
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
     path('<str:artist_slug>/', ArtistDetailView.as_view(), name='artist_detail'),
     path('<str:artist_slug>/<str:album_slug>/', AlbumDetailView.as_view(), name='album_detail'),
 ]
