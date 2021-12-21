cbc9e0bc98bef3e7367685243502f5147d2cb30f
Nikolay Varfolomeev <varnikgen@yandex.ru>
2021-12-21 10:34:41 +1000

    add views for login and registration

--- musicshop/views.py
+++ musicshop/views.py
@@ -1,7 +1,11 @@
 from django import views
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from .models import Artist, Album
+from .models import Artist, Album, Customer
+from .forms import LoginForm, RegistrationForm
+
 
 class BaseView(views.View):
     """Базовое представление"""
@@ -26,3 +30,63 @@ class AlbumDetailView(views.generic.DetailView):
     template_name = 'album/artist_detail.html'
     slug_url_kwarg = 'album_slug'
     context_object_name = 'album'
+
+
+class LoginView(views.View):
+    """Представление формы для авторизации"""
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        context = {
+            'form': form
+        }
+        return render(request, 'login.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.changed_data['username']
+            password = form.changed_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        context = {
+            'form': form
+        }
+        return render(request, 'registration.html', context)
+
+
+class RegistrationView(views.View):
+    """Представление формы для регистрации"""
+
+    def get(self, request, *args, **kwargs):
+        form = RegistrationView(request.POST or None)
+        context = {
+            'form': form
+        }
+        return render(request, 'registration.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.username = form.cleaned_data['username']
+            new_user.email = form.cleaned_data['email']
+            new_user.first_name = form.cleaned_data['first_name']
+            new_user.last_name = form.cleaned_data['last_name']
+            new_user.save()
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            Customer.objects.create(
+                user=new_user,
+                phone=form.cleaned_data['phone'],
+                address=form.cleaned_data['address'],
+            )
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            login(request, user)
+            return HttpResponseRedirect('/')
+        context = {
+            'form': form
+        }
+        return render(request, 'registration.html', context)
